4505466859c93cf6bc5cf9cad7f1a0072c6254f0
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-07 22:20:26 +0500

    pattern 2 and added some comments

--- Displaying Triangles/Form1.cs
+++ Displaying Triangles/Form1.cs
@@ -27,9 +27,21 @@ namespace Displaying_Triangles
                 {
                     pattern1 += "*";
                 }
+
                 pattern1 += "\n";
             }
             MessageBox.Show(pattern1, "Pattern 1");
+
+            string pattern2 = "";
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    pattern2 += "*";
+                }
+                pattern2 += "\n";
+            }
+            MessageBox.Show(pattern2, "Pattern 2");
         }
     }
 }
