5d629d9fbf8df07c7b502c9390248d59f1503f57
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-07 11:22:55 +0500

    Add project files.

---
+++ Displaying Triangles/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Displaying_Triangles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(302, 70);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(161, 42);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Pattern 1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(302, 235);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(161, 42);
+            this.btn3.TabIndex = 1;
+            this.btn3.Text = "Pattern 3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(302, 316);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(161, 42);
+            this.btn4.TabIndex = 2;
+            this.btn4.Text = "Pattern 4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(302, 146);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(161, 42);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Pattern 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ Displaying Triangles/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Displaying_Triangles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pattern1 = "";
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    pattern1 += "*";
+                }
+                pattern1 += "\n";
+            }
+            MessageBox.Show(pattern1, "Pattern 1");
+        }
+    }
+}
